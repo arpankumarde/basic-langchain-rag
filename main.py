2dd76554a46a5d453be0c25f6f17247488ab71d9
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-10-14 22:21:06 +0530

    feat: basic rag implementation

--- main.py
+++ main.py
@@ -1,6 +1,62 @@
-def main():
-    print("Hello from basic-langchain-rag!")
+import getpass
+import os
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_core.documents import Document
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain.chat_models import init_chat_model
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+if not os.environ.get("GOOGLE_API_KEY"):
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+
+llm = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+vector_store = InMemoryVectorStore(embeddings)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+with open("data.txt", "r", encoding="utf-8") as file:
+    data = file.read()
+
+docs = [Document(page_content=data)]
+
+all_splits = text_splitter.split_documents(docs)
+
+vector_store.add_documents(documents=all_splits)
+
+prompt = hub.pull("rlm/rag-prompt")
+
+
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+
+# Define application steps
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+
+# Compile application and test
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+response = graph.invoke({"question": "Are cats friendly?"})
+print(response["answer"])
